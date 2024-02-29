496975a4279831f0b283db319d1f4679ca232411
RubenDavidAbreu <rubendavid954@gmail.com>
2024-02-29 14:29:12 +0100

    Merci Jérôme pour le GET

--- controllers/boxController.js
+++ controllers/boxController.js
@@ -1,43 +1,40 @@
-const { PrismaClient } = require('@prisma/client');
+const { PrismaClient } = require("@prisma/client");
 const prisma = new PrismaClient();
 
 exports.getAllBoxes = async (req, res) => {
   try {
     const allBoxes = await prisma.box.findMany({
       include: {
-        boxaliment: {
-          include: {
-            aliment: true,
-          },
-        },
+        aliment: true,
         flavor: true,
       },
     });
-    var camion = allBoxes.map(box => {
-        const alim = box.boxaliment.map(alim => {
-            const alim2 =  alim.aliment
-            const quantity = alim.quantity
-            return {
-                name: alim2.nom,
-                quantity: quantity
-            }
-        })
-        const flavor = box.flavor
-        const boxes = box.boxaliment
-        res.status(404).send(`aliment ${alim}, flavor ${flavor}`)
-        return {
-            ...boxes,
-            aliment: alim,
-            flavor: flavor
+    const camion = allBoxes.map(box => {
+      const aliment = box.aliment.map((alim) => {
+        if (alim) {
+          return {
+            nom: alim.nom,
+            quantity: alim.quantity,
+          };
         }
-    })
+      });
+      const flavor = box.flavor.map((flav) => {
+        if (flav) {
+          return {
+            nom: flav.nom,
+          };
+        }
+      });
+      return {
+        ...box,
+        aliment: aliment,
+        flavor: flavor,
+      };
+    });
 
-    res.send(`<pre>${JSON.stringify(camion, null, 2)}</pre>`)
+    res.send(`<pre>${JSON.stringify(camion, null, 1)}</pre>`);
     // Transformez vos données ici si nécessaire
-
-    
   } catch (error) {
-    
-    res.status(500).send("Erreur lors de la récupération des boîtes");
+    res.status(500).send(`Erreur lors de la récupération des boîtes ${error}`);
   }
 };
--- index.js
+++ index.js
@@ -1,23 +1,18 @@
 // Importation des modules Express(serveur web), PrismaClient(l'interaction avec la base de données), path(travailler avec les chemins de fichiers)
 const express = require('express');
 const boxRoutes = require('./routes/boxRoutes');
-const path = require('path');
+
 const boxController = require('./controllers/boxController');
 
 // Initialisation Express
 const app = express();
-
-// Configuration d'Express (EJS comme moteur de template)
-app.set('view engine', 'ejs');
-// Définition du chemin vers le répertoire où sont stockés les fichiers de vue EJS
-app.set('views', path.join(__dirname, 'views'));
-
 app.use('/boxes', boxRoutes); //Utilisation d'un "Routeur"
 
 
 
 //utilisation et définition du chemin des routeurs
 app.get('/', boxController.getAllBoxes);
+// app.post('/', boxController.AllBoxes);
 // Démarrage du serveur sur le port 3000
 app.listen(3000, () => {
   console.log('Server is running on http://localhost:3000'); // Confirmation du démarrage du serveur
